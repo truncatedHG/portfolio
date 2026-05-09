0037021fa7a83fd1d8a64adda8a3f38fc44cedc3
Aldrian Diaz <aldriandiaz123@gmail.com>
2026-05-09 10:01:14 +0800

    edit script, add image to tech-stack

--- script/script.js
+++ script/script.js
@@ -1,34 +1,21 @@
-import { animate, utils, createDraggable, spring } from 'animejs';
+document.addEventListener('DOMContentLoaded', () => {
 
-const [ $logo ] = utils.$('.logo.js');
-const [ $button ] = utils.$('button');
-let rotations = 0;
+// Get all "navbar-burger" elements
+const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-// Created a bounce animation loop
-animate('.logo.js', {
-  scale: [
-    { to: 1.25, ease: 'inOut(3)', duration: 200 },
-    { to: 1, ease: spring({ bounce: .7 }) }
-  ],
-  loop: true,
-  loopDelay: 250,
+// Add a click event on each of them
+$navbarBurgers.forEach( el => {
+el.addEventListener('click', () => {
+
+// Get the target from the "data-target" attribute
+const target = el.dataset.target;
+const $target = document.getElementById(target);
+
+// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+el.classList.toggle('is-active');
+$target.classList.toggle('is-active');
+
+});
 });
 
-// Make the logo draggable around its center
-createDraggable('.logo.js', {
-  container: [0, 0, 0, 0],
-  releaseEase: spring({ bounce: .7 })
 });
-
-// Animate logo rotation on click
-const rotateLogo = () => {
-  rotations++;
-  $button.innerText = `rotations: ${rotations}`;
-  animate($logo, {
-    rotate: rotations * 360,
-    ease: 'out(4)',
-    duration: 1500,
-  });
-}
-
-$button.addEventListener('click', rotateLogo);
